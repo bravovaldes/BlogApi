8ff8d328ec3637958b976b847e5c5b3509fe6c2f
bravovaldes <prauvaldes@gmail.com>
2025-05-16 15:51:45 -0400

    Projet terminee

--- obj/Debug/net9.0/BlogApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlogApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ffe33c73987cde6ac7f7d21273da2cc016d19ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
